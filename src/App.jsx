3d67a7f565b96b1a218f4a557a17b174327192a8
Piyush8261 <2022ceb1019@iitrpr.ac.in>
2025-11-19 11:49:01 +0530

    loginSetup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
-import Signup from "./pages/Signup";
-
+import React from 'react'
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import {LoginPage} from './pages/login'
+import './App.css'
+//import home from './pages/home';
+import { HomePage } from './pages/home';
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div>
-      <Signup />
-    </div>
-  );
+    <>
+   {/* <a href="http://localhost:3000/auth/google">
+    <button>Sign in with Google</button>
+</a>*/}
+
+       <BrowserRouter>
+        <Routes>
+          
+       { /*<Route path='/' element={<HomePage></HomePage>}></Route>*/}
+          <Route path='/login' element={<LoginPage></LoginPage>}></Route>
+         <Route path='/' element={<HomePage/>}></Route>
+                
+        </Routes>
+      </BrowserRouter>
+     </>
+  ) 
 }
 
-export default App;
+export default App
